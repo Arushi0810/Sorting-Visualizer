1bd0acbaff578dd8c7d729c46b7b5c16aae749b2
Arushi Agarwal <85348395+Arushi0810@users.noreply.github.com>
2022-01-10 20:28:40 +0530

    Add files via upload

---
+++ js_files/bubble.js
@@ -0,0 +1,21 @@
+async function bubble() {
+  const special = document.getElementsByClassName("bars");
+  for (let i = 0; i < special.length - 1 ; i++) {
+    for(let j = 0 ; j < special.length - i -1  ; j++){
+      special[j].style.background="#267fc5";
+      special[j+1].style.background="#267fc5";
+          if(parseInt(special[j].style.height) > parseInt(special[j+1].style.height)){
+            await new Promise(resolve => setTimeout(() => {resolve(), 10000}));
+              swap(special[j],special[j+1]);
+          }
+          special[j].style.background="#11a3ae";
+          special[j+1].style.background="#11a3ae";
+      } 
+      
+      special[special.length-1-i].style.background = '#708d81';
+    }    
+    special[0].style.background = "#708d81";
+}
+document.getElementsByClassName("bubble sort")[0].addEventListener("click", async function (){
+  await bubble();
+});
