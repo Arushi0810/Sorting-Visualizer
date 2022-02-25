f60e8ed38c6871fc3d3a43cd09d7359b8fc49621
Arushi Agarwal <85348395+Arushi0810@users.noreply.github.com>
2022-02-25 17:21:19 +0530

    final

--- js_files/bubble.js
+++ js_files/bubble.js
@@ -4,19 +4,21 @@ async function bubble() {
     for(let j = 0 ; j < special.length - i -1  ; j++){
      
           if(parseInt(special[j].style.height) > parseInt(special[j+1].style.height)){
-            special[j].style.background="#267fc5";
-            special[j+1].style.background="#267fc5";
-            await new Promise(resolve => setTimeout(() => {resolve(), 100000}));
+            special[j].style.background="#EE9595";
+            special[j+1].style.background="#EE9595";
+            await new wait(delay);
               swap(special[j],special[j+1]);
           }
-          special[j].style.background="#11a3ae";
-          special[j+1].style.background = "#11a3ae";
+          special[j].style.background="#267fc5";
+          special[j+1].style.background = "#267fc5";
       } 
       
-      special[special.length-1-i].style.background = '#708d81';
+      special[special.length-1-i].style.background = '#74c7b8';
     }    
-    special[0].style.background = "#708d81";
+    special[0].style.background = "#74c7b8";
 }
 document.getElementsByClassName("bubble sort")[0].addEventListener("click", async function (){
+  disable();
   await bubble();
+  enable();
 });
--- js_files/selection.js
+++ js_files/selection.js
@@ -2,29 +2,31 @@ async function selection() {
     const special = document.getElementsByClassName("bars");
     for (let i = 0; i < special.length - 1 ; i++) {
         let k = i;
-        special[i].style.background = "#267fc5";
+        special[i].style.background = "#EE9595";
       for(let j = i +1 ; j < special.length  ; j++){
-           special[j].style.background="red";
-        await new Promise(resolve => setTimeout(() => {resolve(), 100000}));
+           special[j].style.background="#EE9565";
+        await wait(delay);
             if(parseInt(special[j].style.height) < parseInt(special[k].style.height)){
                 if(k!== i){
-                    special[k].style.background = "#11a3ae";
+                    special[k].style.background = "#267fc5";
                 }
                 k = j;
             } 
             else{
-                special[j].style.background = "#11a3ae";
+                special[j].style.background = "#267fc5";
             }              
         }
-        await new Promise(resolve => setTimeout(() => {resolve(), 100000}));
+        await wait(delay);
         swap(special[i],special[k]);
-        special[k].style.background = "#11a3ae"
-        special[i].style.background = "#708d81";
+        special[k].style.background = "#267fc5"
+        special[i].style.background = "#74c7b8";
     }    
     
-    special[special.length-1].style.background = "#708d81"; 
+    special[special.length-1].style.background = "#74c7b8"; 
   }
   document.getElementsByClassName("selection sort")[0].addEventListener("click", async function (){
+    disable();
     await selection();
+    enable();
 });
   
